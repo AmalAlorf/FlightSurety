dbc5e9865414a899006efb2f5f12c0591aeb8dfe
Amal Alorf <amal.alorf@gmail.com>
2019-08-19 21:19:41 +0300

    FlightSurety

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,23 @@
+const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
+const fs = require('fs');
+
+module.exports = function(deployer) {
+
+    let firstAirline = '0xe2cbacb5cfe7d1092e7959c9fac44aaa9d16727e';
+    deployer.deploy(FlightSuretyData, firstAirline)
+        .then(() => {
+            return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
+                .then(() => {
+                    let config = {
+                        localhost: {
+                            url: 'http://localhost:8545',
+                            dataAddress: FlightSuretyData.address,
+                            appAddress: FlightSuretyApp.address
+                        }
+                    }
+                    fs.writeFileSync(__dirname + '/../src/dapp/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+                    fs.writeFileSync(__dirname + '/../src/server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+                });
+        });
+}
